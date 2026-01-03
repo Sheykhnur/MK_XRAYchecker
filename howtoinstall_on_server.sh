e01c5fcbd83baa0b3dabfc721166649042f25007
A9FM [aka Artur Lamerov] <143527536+a9-fm@users.noreply.github.com>
2026-01-03 14:15:58 +0300

    Update howtoinstall_on_server.sh

--- howtoinstall_on_server.sh
+++ howtoinstall_on_server.sh
@@ -1,1 +1,1 @@
-apt update -y ; apt upgrade -y ; apt install python3 python3-pip git curl -y ; git clone https://github.com/EtoNeYaProject/MK_XRAYchecker ; cd MK_XRAYchecker ; python3 bot.py
+apt update -y ; apt upgrade -y ; apt install python3 python3-pip git curl -y ; git clone https://github.com/EtoNeYaProject/MK_XRAYchecker ; cd MK_XRAYchecker ; pip3 install -r requirements.txt ; clear ; python3 bot.py
